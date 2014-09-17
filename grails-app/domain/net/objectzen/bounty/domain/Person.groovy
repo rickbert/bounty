27881adc9ffc11c4532fa2e835b31542e0cdea24
Rick Cochrane <rick.cochrane@gmail.com>
2014-09-17 10:43:45 -0500

    - now we have a total earned field.

--- grails-app/domain/net/objectzen/bounty/domain/Person.groovy
+++ grails-app/domain/net/objectzen/bounty/domain/Person.groovy
@@ -5,8 +5,12 @@ class Person {
     String name
     String email
 
+    float totalEarned
+
     static constraints = {
         name blank: false
         email blank: false, email: true
+
+        totalEarned min: 0.0f
     }
 }
